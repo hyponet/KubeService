6af3cb9dc3abe0d20b2ad6bda88deb4460985041
Hypo <i@ihypo.net>
2019-07-26 16:26:34 +0800

    add ms ctrl

--- pkg/controller/microservice/instance.go
+++ pkg/controller/microservice/instance.go
@@ -48,7 +48,7 @@ func (r *ReconcileMicroService) reconcileInstance(microService *appv1.MicroServi
 func makeVersionDeployment(version *appv1.DeployVersion, microService *appv1.MicroService) (*appsv1.Deployment, error) {
 
 	labels := microService.Labels
-	if labels==nil{
+	if labels == nil {
 		labels = make(map[string]string)
 	}
 	labels["app.o0w0o.cn/service"] = microService.Name
